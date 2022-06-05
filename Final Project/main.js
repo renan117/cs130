93a6154b2bfcf078813a1cf37bfad15ba42161e6
renan117 <renandennig2023@u.northwestern.edu>
2022-06-05 18:11:41 -0500

    project

---
+++ Final Project/main.js
@@ -0,0 +1,74 @@
+let currentIndex = 0;
+
+const images = [
+    'images/fam1.jpeg',
+    'images/rwdo.jpg',
+    'images/grad1.jpeg',
+    'images/grad2.jpeg',
+    'images/rwd.jpg',
+    'images/rwm.jpg',
+    'images/rwy.jpg',
+    'images/thanks.jpg'
+];
+
+const initScreen = () => {
+    images.forEach((image, idx) => {
+        document.querySelector('.cards').innerHTML += `
+        <li class="card">
+            <button class="image" 
+                onclick="handleThumbnailClick(event)"
+                style="background-image:url('${image}')"
+                data-index="${idx}"
+                aria-label="Displays image ${idx} in the main panel."></button>
+        </li>`;
+    });
+};
+
+initScreen();
+
+const showImage = (ev) =>{
+    const elem=ev.currentTarget;
+    currentIndex = parseInt(elem.dataset.index);
+}
+
+const handleThumbnailClick = ev =>{
+    const elem = ev.currentTarget;
+    console.log(ev);
+    currentIndex = parseInt(elem.dataset.index);
+    const bgImage = elem.style.backgroundImage;
+    document.querySelector('.featured_image').style.backgroundImage =bgImage;
+    currentIndex = Number(elem.getAttribute('data-index'));
+    console.log(currentIndex)
+}
+
+const handleNextClick = ev =>{
+    if (currentIndex<7) {
+        currentIndex +=1;
+    }
+    else {
+        currentIndex=0;
+    }
+    console.log("next")
+    console.log(currentIndex)
+    document.querySelector('.featured_image').style.backgroundImage = 
+    `url("${images[currentIndex]}")`;
+}
+
+const handlePrevClick = ev =>{
+    if (currentIndex>0) {
+        currentIndex -=1;
+    }
+    else {
+        currentIndex=7;
+    }
+    console.log("prev")
+    console.log(currentIndex)
+    document.querySelector('.featured_image').style.backgroundImage = 
+    `url("${images[currentIndex]}")`;
+}
+
+document.querySelector(".next").onclick = handleNextClick;
+document.querySelector(".featured_image").onclick = handleNextClick;
+document.querySelector(".prev").onclick = handlePrevClick;
+
+
